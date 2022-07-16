0db1349a92af53473e9a2af4ec8f290be89820bd
panda1 <blueblue0011@gmail.com>
2022-07-16 07:54:46 -0700

    not quite working

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-// using querySelector() to target header and adding another line to appear after "hit there"
+// using querySelector() to target header and adding another line to appear after "hi there"
 let header_background = document.querySelector(`#for_header`);
 header_background[`outerHTML`] += `<h1> This should appear after original Hi there</h1>`;
 
-// using querySelector()
-let header_text = document.querySelector(`#for_header_title`);
+// using getElementById to change background color of the text box
+let header_text = document.getElementById(`for_header_title`);
 header_text[`style`][`backgroundColor`] = 'orange';
 
-// using querySelectorAll()
+// using querySelectorAll() replacing p tag content
 
-let p_background = document.querySelectorAll(`.for_p`);
-p_background[`innerHTML`] = ` <a href = "https://images.unsplash.com/photo-1596130619401-d822f9dede1b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c21hbGwlMjBkdWNrfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60">
-a duck</a>`;
+let p_text = document.querySelectorAll(`.for_main`);
+p_text[`innerHTML`] = `replacing original`;
+
+// using getElementsByClassName() content before current content
+let p_all = document.getElementsByClassName(`for_p`);
+p_all[`outerHTML`] = `<inline>this is before</inline>` + p_all[`outerHTML`];
